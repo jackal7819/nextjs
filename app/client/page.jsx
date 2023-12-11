8c677373ecc671290841bcae9171b4f504872240
Viktor Filippov <jackal3166@gmail.com>
2023-12-11 17:54:30 +0200

    Create Client

--- app/client/page.jsx
+++ app/client/page.jsx
@@ -1,12 +1,35 @@
-import Link from 'next/link';
+'use client';
+
+import { useState } from 'react';
 
 const ClientPage = () => {
+	const [count, setCount] = useState(0);
+
+	const plusHandler = () => {
+		setCount((prev) => prev + 1);
+	};
+
+	const minusHandler = () => {
+		setCount((prev) => prev - 1);
+	};
+
 	return (
 		<div>
-			<h1 className='text-3xl font-bold underline'>Hello Client!</h1>
-			<Link href='/' className='text-2xl underline'>
-				Home Page
-			</Link>
+			<h1 className='my-20 font-bold text-center text-7xl'>{count}</h1>
+			<div className='flex justify-center gap-20'>
+				<button
+					className='text-2xl btn btn-primary'
+					onClick={plusHandler}
+				>
+					+
+				</button>
+				<button
+					className='text-2xl btn btn-primary '
+					onClick={minusHandler}
+				>
+					-
+				</button>
+			</div>
 		</div>
 	);
 };
