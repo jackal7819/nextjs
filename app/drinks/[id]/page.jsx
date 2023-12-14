b64827f1da593130485dfa6347b34eb9e1ed9e48
Viktor Filippov <jackal3166@gmail.com>
2023-12-14 21:32:28 +0200

    Add Image

--- app/drinks/[id]/page.jsx
+++ app/drinks/[id]/page.jsx
@@ -1,7 +1,52 @@
-const SingleDrinkPage = ({ params }) => {
-	console.log(params.id);
+import Image from 'next/image';
+import Link from 'next/link';
+import drinkImg from './drink.jpg';
 
-	return <div>SingleDrinkPage</div>;
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=';
+
+const getSingleDrink = async (id) => {
+	const response = await fetch(`${url}${id}`);
+
+	if (!response.ok) {
+		throw new Error('Failed to fetch a drink');
+	}
+
+	const data = await response.json();
+	return data;
+};
+
+const SingleDrinkPage = async ({ params }) => {
+	const { id } = params;
+	const data = await getSingleDrink(id);
+	const title = data?.drinks[0]?.strDrink;
+	const image = data?.drinks[0]?.strDrinkThumb;
+	const instructions = data?.drinks[0]?.strInstructions;
+
+	return (
+		<div>
+			<Link
+				href='/drinks'
+				className='mt-8 mb-12 uppercase btn btn-primary'
+			>
+				back to drinks
+			</Link>
+			{/* <Image
+				src={drinkImg}
+				alt='drink'
+				className='w-48 h-48 mb-8 rounded-lg'
+			/> */}
+			<Image
+				src={image}
+                width={300}
+                height={300}
+				alt={title}
+                priority
+				className='w-48 h-48 mb-8 rounded-lg shadow-lg'
+			/>
+			<h1 className='mb-8 text-4xl'>{title}</h1>
+			<p className='mb-8 text-xl'>{instructions}</p>
+		</div>
+	);
 };
 
 export default SingleDrinkPage;
--- components/DrinksList.jsx
+++ components/DrinksList.jsx
@@ -1,15 +1,25 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const DrinksList = ({ drinks }) => {
 	return (
-		<ul className='menu menu-vertical'>
+		<ul className='grid gap-6 mt-6 sm:grid-cols-2'>
 			{drinks.map((drink) => (
 				<li key={drink.idDrink}>
 					<Link
 						href={`/drinks/${drink.idDrink}`}
 						className='text-xl font-medium'
 					>
-						{drink.strDrink}
+						<div className='relative h-48 mb-4'>
+							<Image
+								src={drink.strDrinkThumb}
+								fill
+								sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw'
+								alt={drink.strDrink}
+								className='object-cover rounded-md'
+							/>
+						</div>
+						<h2 className='text-center'>{drink.strDrink}</h2>
 					</Link>
 				</li>
 			))}
