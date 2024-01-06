e3b7a7e90a8d0f75db6dae21b5f52bddce0c2e5d
Viktor Filippov <jackal3166@gmail.com>
2024-01-06 02:36:58 +0200

    Add route

---
+++ app/api/tasks/route.js
@@ -0,0 +1,13 @@
+import { NextResponse } from 'next/server';
+import db from '../../../services/db';
+
+export const GET = async (request) => {
+	const tasks = await db.task.findMany();
+	return NextResponse.json({ data: tasks });
+};
+
+export const POST = async (request) => {
+	const data = await request.json();
+	const task = await db.task.create({ data: { content: data.content } });
+	return NextResponse.json({ data: task });
+};
